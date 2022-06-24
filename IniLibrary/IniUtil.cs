c583704697ad90452e68e6d0c6145c0ebfc44a05
Vladimir Kosickij <vladimir.kosickij@gmail.com>
2022-06-24 19:42:55 +0300

    Add project files.

---
+++ IniLibrary/IniUtil.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IniLibrary
+{
+    public class IniFile
+    {
+        string Path;
+        string EXE = Assembly.GetExecutingAssembly().GetName().Name;
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+        static extern long WritePrivateProfileString(
+            string Section,
+            string Key,
+            string Value,
+            string FilePath
+        );
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+        static extern int GetPrivateProfileString(
+            string Section,
+            string Key,
+            string Default,
+            StringBuilder RetVal,
+            int Size,
+            string FilePath
+        );
+        /// <summary>
+        /// Constructor for IniFile class
+        /// </summary>
+        /// <param name="IniPath">Path to INI file</param>
+        public IniFile(string IniPath = null)
+        {
+            Path = new FileInfo(IniPath ?? EXE + ".ini").FullName;
+        }
+        /// <summary>
+        /// Returns value from ini file by key and section
+        /// </summary>
+        /// <param name="Key">Ini file key</param>
+        /// <param name="Section">Ini file section</param>
+        /// <returns></returns>
+        public string Read(string Key, string Section = null)
+        {
+            var RetVal = new StringBuilder(255);
+            GetPrivateProfileString(Section ?? EXE, Key, "", RetVal, 255, Path);
+            return RetVal.ToString();
+        }
+        /// <summary>
+        /// Writes value to ini file by key and section
+        /// </summary>
+        /// <param name="Key">Ini file key</param>
+        /// <param name="Value">Writing value</param>
+        /// <param name="Section">Ini file section</param>
+        public void Write(string Key, string Value, string Section = null)
+        {
+            WritePrivateProfileString(Section ?? EXE, Key, Value, Path);
+        }
+        /// <summary>
+        /// Removes value from ini file by key and section
+        /// </summary>
+        /// <param name="Key">Ini file key</param>
+        /// <param name="Section">Ini file section</param>
+        public void DeleteKey(string Key, string Section = null)
+        {
+            Write(Key, null, Section ?? EXE);
+        }
+        /// <summary>
+        /// Checks if key from ini file exists by key and section
+        /// </summary>
+        /// <param name="Key">Ini file key</param>
+        /// <param name="Section">Ini file section</param>
+        /// <returns></returns>
+        public bool KeyExists(string Key, string Section = null)
+        {
+            return Read(Key, Section).Length > 0;
+        }
+    }
+}
---
+++ IniLibrary/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("IniLibrary")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IniLibrary")]
+[assembly: AssemblyCopyright("Vladimir Kosickij ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9d71ae1f-cc8e-4c0c-8b80-a7587e0619f8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
